10a5325e77f39d6793992955c490d25f5bd6b83e
Andrianjaka-Tony <andrianjaka.tony@gmail.com>
2026-07-28 22:37:07 +0300

    Render the testimonials and FAQ sections

--- src/modules/home/hone.page.tsx
+++ src/modules/home/hone.page.tsx
@@ -2,11 +2,13 @@ import { BreakTheStatic } from "@/modules/home/components/break-the-staic";
 import { DearFutureDancer } from "@/modules/home/components/dear-future-dancer";
 import { Different } from "@/modules/home/components/different";
 import { Escape } from "@/modules/home/components/escape";
+import { Faq } from "@/modules/home/components/faq";
 import { Footer } from "@/modules/home/components/footer";
 import { Hero } from "@/modules/home/components/hero";
 import { Look } from "@/modules/home/components/look";
 import { Stay } from "@/modules/home/components/stay";
 import { Teachers } from "@/modules/home/components/teachers";
+import { Testimonials } from "@/modules/home/components/testimonials";
 import { Fragment } from "react/jsx-runtime";
 
 export function HomePage() {
@@ -18,6 +20,8 @@ export function HomePage() {
       <DearFutureDancer />
       <Stay />
       <Teachers />
+      <Testimonials />
+      <Faq />
       {/* <Escape />
       <Different /> */}
       <Footer />
